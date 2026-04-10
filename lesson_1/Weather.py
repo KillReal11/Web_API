0724c8dc4296d9faf3c96de31a84237728f542d8
Kirill <dawiduk2013@yandex.ru>
2026-04-11 00:41:53 +0500

    Update Weather.py

--- lesson_1/Weather.py
+++ lesson_1/Weather.py
@@ -1,14 +1,18 @@
 import requests
 
 
-def main():
-    locations = ["london", "svo", "Череповец"]
-    for loc in locations:
-        url = f'https://wttr.in/{loc}?MTnq&lang=ru'
+def main(locations: list) -> list:
+    forecast_data = []
+    for location in locations:
+        url = f'https://wttr.in/{location}?MTnq&lang=ru'
         response = requests.get(url)
         response.raise_for_status()
-        print(response.text)
+        forecast_data.append(response.text)
+    return forecast_data
 
 
 if __name__ == '__main__':
-    main()
+    locations = ["london", "svo", "Череповец"]
+    forecast_data = main(locations)
+    for location in forecast_data:
+        print(location)
