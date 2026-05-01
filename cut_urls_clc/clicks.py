3136995501b5d565004b4726d9b0d10560142d88
Kirill <dawiduk2013@yandex.ru>
2026-05-01 18:50:36 +0500

    Обрезка ссылок с помощью CLC

---
+++ cut_urls_clc/clicks.py
@@ -0,0 +1,96 @@
+import requests
+from dotenv import load_dotenv
+import os
+import argparse
+
+
+def createParser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('long_url', help='введите ссылку, которую хотите сократить')
+    args = parser.parse_args()
+    long_url = args.long_url
+    return long_url
+
+def shorten_link(token, long_url):
+    url = 'https://clc.li/api/url/add'
+    headers = {
+        'Authorization': token,
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'url': long_url,
+        "status": "public"
+    }
+    response = requests.post(url, headers=headers, json=data)
+    response.raise_for_status()
+    answer = response.json()
+    error = answer.get('error')
+    if error == 1:
+        return None
+    short_url_full = answer.get('shorturl')
+    return short_url_full
+
+
+def count_clicks(token, long_url):
+    url = 'https://clc.li/api/urls'
+    headers = {
+        'Authorization': token,
+        'Content-Type': 'application/json'
+    }
+    params = {
+        'limit': '2',
+        'page': '1',
+        'order': 'date',
+        'q': long_url
+    }
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    answer = response.json()
+    main_part = answer.get('data')
+    urls = main_part.get('urls')
+    for link in urls:
+        if link.get('longurl') != long_url:
+            return None
+        overall_clicks = link.get('clicks')
+        unique_clicks = link.get('uniqueclicks')
+        return overall_clicks, unique_clicks
+
+
+def is_bitlink(token, long_url):
+    url = 'https://clc.li/api/urls'
+    headers = {
+        'Authorization': token,
+        'Content-Type': 'application/json'
+    }
+    params = {
+        'limit': '2',
+        'page': '1',
+        'order': 'date',
+        'q': long_url
+    }
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    answer = response.json()
+    response_data = answer.get('data')
+    urls = response_data.get('urls')
+    return urls[0].get('longurl') == long_url
+
+
+def main():
+    load_dotenv()
+    token = os.environ['CLC_API_TOKEN']
+    long_url = createParser()
+    if is_bitlink(token, long_url):
+        clicks_number = count_clicks(token, long_url=long_url)
+        print('Всего кликов:', clicks_number[0])
+        print('Уникальных кликов:', clicks_number[1])
+    else:
+        short_url = shorten_link(token, long_url)
+        if short_url:
+            print('Новая короткая ссылка -->', short_url)
+        else:
+            print('Вы ввели некорректную ссылку!')
+
+
+if __name__ == '__main__':
+    main()
